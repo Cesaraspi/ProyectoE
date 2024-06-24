5f12ef07e9c1d4f3dfb5388ab48411ed738752cb
Cesaraspi <135297564+Cesaraspi@users.noreply.github.com>
2024-06-24 11:07:06 -0400

    Add files via upload
    Entrega de proyecto

---
+++ ProyectoE/Ventas/src/main/java/Config/Conexion.java
@@ -0,0 +1,43 @@
+
+package Config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Conexion {
+    static public String driver = "com.mysql.cj.jdbc.Driver";
+    static public String url ="jdbc:mysql://localhost:3306/bdventas";
+    static public String usuario = "root";
+    static public String password = "";
+    protected Connection conn = null;
+
+    public Conexion() {
+        try{
+            Class.forName(driver);
+        conn = DriverManager.getConnection(url,usuario,password);   
+        }catch(ClassNotFoundException ex){
+            
+            System.out.println("Error en el driver:"+ ex.getMessage());
+        }catch(SQLException ex){
+            System.out.println("Error al realizar la conexion"+ ex.getMessage());
+        }
+    }
+public Connection conectar(){
+    return conn;
+}
+public void desconectar(){
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println("Error al cerrar la conexion"+ ex.getMessage());
+        }
+}
+ 
+    
+           
+    
+}
